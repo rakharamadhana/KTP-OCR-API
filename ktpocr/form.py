542434882f67bc5961c6ddf90f151bc746bd39a1
Rakha Ramadhana <rakha.ramadhana@yahoo.com>
2024-09-26 08:23:45 +0800

    Ordered json

--- ktpocr/form.py
+++ ktpocr/form.py
@@ -1,3 +1,7 @@
+from collections import OrderedDict
+import json
+
+
 class KTPInformation(object):
     def __init__(self):
         self.nik = ""
@@ -15,4 +19,27 @@ class KTPInformation(object):
         self.status_perkawinan = ""
         self.pekerjaan = ""
         self.kewarganegaraan = ""
-        berlaku_hingga = "SEMUR HIDUP"
+        self.berlaku_hingga = "SEMUR HIDUP"
+
+    def to_ordered_dict(self):
+        return OrderedDict([
+            ("nik", self.nik),
+            ("nama", self.nama),
+            ("tempat_lahir", self.tempat_lahir),
+            ("tanggal_lahir", self.tanggal_lahir),
+            ("jenis_kelamin", self.jenis_kelamin),
+            ("golongan_darah", self.golongan_darah),
+            ("alamat", self.alamat),
+            ("rt", self.rt),
+            ("rw", self.rw),
+            ("kelurahan_atau_desa", self.kelurahan_atau_desa),
+            ("kecamatan", self.kecamatan),
+            ("agama", self.agama),
+            ("status_perkawinan", self.status_perkawinan),
+            ("pekerjaan", self.pekerjaan),
+            ("kewarganegaraan", self.kewarganegaraan),
+            ("berlaku_hingga", self.berlaku_hingga),
+        ])
+
+    def to_json(self):
+        return json.dumps(self.to_ordered_dict(), indent=4)
